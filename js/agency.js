9c0d461227f7ef96f23756003260ad3f05843003
matthewbullock <matthewbullockdev@gmail.com>
2015-07-28 16:48:37 +1200

    fade

--- js/agency.js
+++ js/agency.js
@@ -24,3 +24,19 @@ $('body').scrollspy({
 $('.navbar-collapse ul li a').click(function() {
     $('.navbar-toggle:visible').click();
 });
+
+$('.dropdown').on('show.bs.dropdown', function(e){
+      var $dropdown = $(this).find('.dropdown-menu');
+      var orig_margin_top = parseInt($dropdown.css('margin-top'));
+      $dropdown.css({'margin-top': (orig_margin_top + 10) + 'px', opacity: 0}).animate({'margin-top': orig_margin_top + 'px', opacity: 1}, 300, function(){
+         $(this).css({'margin-top':''});
+      });
+   });
+   // Add slidedown & fadeout animation to dropdown
+   $('.dropdown').on('hide.bs.dropdown', function(e){
+      var $dropdown = $(this).find('.dropdown-menu');
+      var orig_margin_top = parseInt($dropdown.css('margin-top'));
+      $dropdown.css({'margin-top': orig_margin_top + 'px', opacity: 1, display: 'block'}).animate({'margin-top': (orig_margin_top + 10) + 'px', opacity: 0}, 300, function(){
+         $(this).css({'margin-top':'', display:''});
+      });
+   });
