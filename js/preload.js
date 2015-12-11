992f14c3c236fbe141a4a41ff13241f24de7762b
matthewbullock <matthewbullockdev@gmail.com>
2015-12-12 12:27:26 +1300

    Updated website

--- js/preload.js
+++ js/preload.js
@@ -2,3 +2,16 @@
 		// Animate loader off screen
 		$(".se-pre-con").fadeOut("slow");;
 	});
+
+	$(function () {
+    $(window).scroll(function () {
+        var currentScrollTop = $(window).scrollTop();
+		var opacity = currentScrollTop/$('#blackOverlay').height();
+		if(opacity < 0.7) {
+        	$('#blackOverlay').css('opacity',currentScrollTop/$('#blackOverlay').height());
+        }
+        else {
+        	$('#blackOverlay').css('opacity', 0.7);
+        }
+  });
+});
